80e25b71826c322c25e976a1e7168c34150a9f7a
Ylia <ukhrestina@yandex.ru>
2023-04-02 09:53:01 +0300

    показывает вторую цифру трезначного числа

---
+++ task_10/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int a = 0;
+int b = 0;
+int SecondNum = 0;
+System.Console.WriteLine("Введите трехзначное число: ");
+a = int.Parse(Console.ReadLine());
+if (a>=100 && a<1000)
+{
+    b = a / 10;
+    SecondNum = b % 10;
+    System.Console.Write("Второй цифрой этого числа является ");
+    System.Console.WriteLine(SecondNum);
+
+}
+else
+{
+    System.Console.WriteLine("Число некорректное!");
+}
