1ce61d60053c129583daf19f030c0195e4d6cdd8
Ylia <ukhrestina@yandex.ru>
2023-04-08 19:29:48 +0300

    задаёт массив из 8 элементов и выводит их на экран

---
+++ task_29/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+using System;
+class Program 
+{ 
+    static void Main(string[] args) 
+    { 
+        int[] arr = new int[8]; 
+
+        for (int i = 0; i < arr.Length; i++) 
+        { 
+            arr[i] = GetInput($"Введите {i+1}-й элемент массива: "); 
+        } 
+        
+        PrintArray(arr); 
+    } 
+
+static int GetInput(string prompt)  
+    { 
+        Console.Write(prompt); 
+        return int.Parse(Console.ReadLine()); 
+    }
+
+static void PrintArray(int[] arr) 
+    { 
+    Console.WriteLine("Массив:"); 
+
+     for (int i = 0; i < arr.Length; i++) 
+     { 
+        Console.Write(arr[i] + " "); 
+     } 
+    }   
+} 
